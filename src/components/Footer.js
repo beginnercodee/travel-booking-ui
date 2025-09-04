ad280b5b604fed0041a2198d490f6d0b0681bcc5
JamalNadeem <jamalnadeem2004@gmail.com>
2025-09-04 21:55:33 +0500

    feat: add Navbar and Footer components with basic routing

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-dark text-white text-center py-4 mt-5">
+      <p>© 2025 TravelUI. All rights reserved.</p>
+      <div>
+        <a href="#" className="text-white mx-2">Facebook</a>
+        <a href="#" className="text-white mx-2">Twitter</a>
+        <a href="#" className="text-white mx-2">Instagram</a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
